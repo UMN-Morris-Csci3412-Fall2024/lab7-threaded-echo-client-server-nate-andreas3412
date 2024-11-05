cb8d666a037246d8a98e92afc86aaebae46a4df5
AndreasNelson <nel02254@morris.umn.edu>
2024-11-05 12:37:46 -0600

    rewrote echoclient using threads

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -7,17 +7,76 @@ import java.net.Socket;
 
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
+    public static final String SERVER_ADDRESS = "localhost";
 
-	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
+    public static void main(String[] args) throws IOException {
+        EchoClient client = new EchoClient();
+        client.start();
+    }
+
+    private void start() throws IOException {
+        Socket socket = new Socket(SERVER_ADDRESS, PORT_NUMBER);
+        InputStream socketInputStream = socket.getInputStream();
+        OutputStream socketOutputStream = socket.getOutputStream();
+
+        Thread reader = new Thread(new SocketReader(socketInputStream));
+        Thread writer = new Thread(new SocketWriter(socketOutputStream));
+
+        reader.start();
+        writer.start();
+
+        try {
+            writer.join();
+            socket.shutdownOutput();
+            reader.join();
+        } catch (InterruptedException e) {
+            System.err.println("Thread interrupted: " + e);
+        } finally {
+            socket.close();
+        }
+    }
+
+	private static class SocketReader implements Runnable {
+		private final InputStream in;
+
+		public SocketReader(InputStream in) {
+			this.in = in;
+		}
+
+		@Override
+		public void run() {
+			try {
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = in.read(buffer)) != -1) {
+					System.out.write(buffer, 0, bytesRead);
+					System.out.flush(); // Added missing flush
+				}
+			} catch (IOException e) {
+				System.err.println("Error reading from server: " + e);
+			}
+		}
 	}
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+	private static class SocketWriter implements Runnable {
+		private final OutputStream out;
 
-		// Put your code here.
+		public SocketWriter(OutputStream out) {
+			this.out = out;
+		}
+
+		@Override
+		public void run() {
+			try {
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = System.in.read(buffer)) != -1) {
+					out.write(buffer, 0, bytesRead);
+					out.flush();
+				}
+			} catch (IOException e) {
+				System.err.println("Error writing to server: " + e);
+			}
+		}
 	}
 }
